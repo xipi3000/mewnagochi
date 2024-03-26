7ed34fea7d2698d7432a080406184c5acd14652b
Ant <antoniolopez18022002@gmail.com>
2024-03-26 17:21:33 +0100

    fixed typo

--- app/src/main/java/com/projecte/mewnagochi/ui/MainScreen.kt
+++ app/src/main/java/com/projecte/mewnagochi/ui/MainScreen.kt
@@ -1,18 +1,10 @@
 package com.projecte.mewnagochi.ui
 
-import android.view.View
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -20,17 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
-import androidx.lifecycle.ViewModel
-import com.projecte.mewnagochi.LaveledIcon
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.projecte.mewnagochi.LabeledIcon
 import com.projecte.mewnagochi.MyViewModel
 import com.projecte.mewnagochi.R
-
 
 
 @Composable
@@ -38,9 +26,9 @@ fun MainScreen(
     myViewModel : MyViewModel = viewModel()
 ) {
     val selectedItem by myViewModel.navigationBarSelected.collectAsState()
-    val navigationBarItems = listOf(LaveledIcon("Home",Icons.Filled.Home),
-        LaveledIcon("Activities",  ImageVector.vectorResource(id = R.drawable.baseline_directions_run_24)),
-            LaveledIcon("Chats",ImageVector.vectorResource(id = R.drawable.baseline_forum_24))
+    val navigationBarItems = listOf(LabeledIcon("Home",Icons.Filled.Home),
+        LabeledIcon("Activities",  ImageVector.vectorResource(id = R.drawable.baseline_directions_run_24)),
+            LabeledIcon("Chats",ImageVector.vectorResource(id = R.drawable.baseline_forum_24))
             )
     Scaffold(
         bottomBar = {
@@ -48,8 +36,8 @@ fun MainScreen(
                 NavigationBar {
                     navigationBarItems.forEachIndexed { index, item ->
                         NavigationBarItem(
-                            icon = { Icon(item.icon, contentDescription = item.lavel) },
-                            label = { Text(item.lavel) },
+                            icon = { Icon(item.icon, contentDescription = item.label) },
+                            label = { Text(item.label) },
                             selected = selectedItem == index,
                             onClick = { myViewModel.setNewSelected(index) }
                         )
