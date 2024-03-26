da851166abf56b072230847265a958cf4431d3ae
Pol Escolà Curcó <polescola3000@gmail.com>
2024-03-26 19:25:29 +0100

    dinamic routes and screens

--- app/src/main/java/com/projecte/mewnagochi/LabeledIcon.kt
+++ app/src/main/java/com/projecte/mewnagochi/LabeledIcon.kt
@@ -1,11 +1,16 @@
 package com.projecte.mewnagochi
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 class LabeledIcon(
-    label: String,
-    icon : ImageVector
-) {
-    val label = label
-    val icon = icon
-}
+    val label: String,
+    val icon: ImageVector,
+    val screen:  @Composable () -> Unit
+)
--- app/src/main/java/com/projecte/mewnagochi/ui/MainScreen.kt
+++ app/src/main/java/com/projecte/mewnagochi/ui/MainScreen.kt
@@ -1,6 +1,10 @@
 package com.projecte.mewnagochi.ui
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -13,38 +17,64 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.projecte.mewnagochi.LabeledIcon
 import com.projecte.mewnagochi.MyViewModel
 import com.projecte.mewnagochi.R
+import kotlinx.coroutines.selects.select
+
+val homeScreen: @Composable () -> Unit = {
+    HomeScreen()
+}
 
 
 @Composable
 fun MainScreen(
-    myViewModel : MyViewModel = viewModel()
+    myViewModel : MyViewModel = viewModel(),
+    navController : NavHostController = rememberNavController(),
+    navigationBarItems : List<LabeledIcon> =listOf(
+        LabeledIcon("Home", Icons.Filled.Home){ HomeScreen() },
+        LabeledIcon("Activities",  ImageVector.vectorResource(id = R.drawable.baseline_directions_run_24)) { ActivitiesScreen() },
+        LabeledIcon("Chats",ImageVector.vectorResource(id = R.drawable.baseline_forum_24)) { ChatScreen() },
+    )
 ) {
+    @Composable
+    fun MyBox() {
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.Blue)
+        )
+    }
+
+// Define a variable of type @Composable () -> Unit
+    var myComposable: @Composable () -> Unit = { }
+
+// Assign your Box composable to the variable
+    myComposable = { MyBox() }
+
     val selectedItem by myViewModel.navigationBarSelected.collectAsState()
-    val navigationBarItems = listOf(LabeledIcon("Home",Icons.Filled.Home),
-        LabeledIcon("Activities",  ImageVector.vectorResource(id = R.drawable.baseline_directions_run_24)),
-            LabeledIcon("Chats",ImageVector.vectorResource(id = R.drawable.baseline_forum_24))
-            )
+
     Scaffold(
         bottomBar = {
-
                 NavigationBar {
                     navigationBarItems.forEachIndexed { index, item ->
                         NavigationBarItem(
                             icon = { Icon(item.icon, contentDescription = item.label) },
                             label = { Text(item.label) },
                             selected = selectedItem == index,
-                            onClick = { myViewModel.setNewSelected(index) }
+                            onClick = {
+                                myViewModel.setNewSelected(index)
+                                navController.navigate(item.label)
+                            }
                         )
                     }
-
-
-
             }
         }
     ){
@@ -52,24 +82,32 @@ fun MainScreen(
         Column(
             modifier = Modifier.padding(scaffoldPadding)
         ) {
-            when (selectedItem) {
-                0 -> {
-                    HomeScreen()
+
+
+
+            NavHost(
+                navController = navController,
+                startDestination = "Home"
+            ) {
+
+
+                navigationBarItems.forEach { item ->
+                    composable(item.label){
+                        item.screen()
+                    }
                 }
-                1 -> {
-                    ActivitiesScreen()
                 }
-                else -> {
-                    ChatScreen()
-                }
+
             }
         }
 
     }
-}
+
+
 
 @Composable
 fun HomeScreen(){
+    Log.i("gooffy","whaaaa")
     Text(text = "HOME")
 }
 @Composable
