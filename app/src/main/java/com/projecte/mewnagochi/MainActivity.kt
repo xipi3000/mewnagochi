b89a97d5195472d663029b4b19b90c9bd6a548ee
Ant <antoniolopez18022002@gmail.com>
2024-03-28 23:11:08 +0100

    merged with animations

--- app/src/main/java/com/projecte/mewnagochi/MainActivity.kt
+++ app/src/main/java/com/projecte/mewnagochi/MainActivity.kt
@@ -5,12 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.projecte.mewnagochi.ui.MainScreen
-import com.projecte.mewnagochi.ui.animation.Animation
 import com.projecte.mewnagochi.ui.theme.MewnagochiTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +18,9 @@ class MainActivity : ComponentActivity() {
             MewnagochiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen(context=this, scope = this.lifecycleScope)
+                    MainScreen(context = this, scope = this.lifecycleScope)
 
                 }
             }
