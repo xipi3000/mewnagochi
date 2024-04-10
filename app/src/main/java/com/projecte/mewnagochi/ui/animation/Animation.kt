9b900c341bbace8bfe469f9a8027f4d0488c6274
Ant <antoniolopez18022002@gmail.com>
2024-04-10 18:55:56 +0200

    final touches

--- app/src/main/java/com/projecte/mewnagochi/ui/animation/Animation.kt
+++ app/src/main/java/com/projecte/mewnagochi/ui/animation/Animation.kt
@@ -1,7 +1,6 @@
 package com.projecte.mewnagochi.ui.animation
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +12,9 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 
 
 class Animation(
@@ -50,18 +48,13 @@ class Animation(
         ) {
             Canvas(
                 modifier = modifier
-                    .width(Dp(100F))
-                    .height(Dp(180f))
-                    .background(Color.Red)
+                    .width(frames[frameIndex.value].width.dp/5)
+                    .height(frames[frameIndex.value].height.dp*10/53)
+                    //.clipToBounds()
             ) {
-                drawImage(
-                    frames[frameIndex.value],
-//                    dstSize = IntSize(
-//                        frames[frameIndex.value].width / 2, frames[frameIndex.value].height / 2
-//                    ),
-                    //Canvas size * 3.5
-                    dstSize = IntSize (frames[frameIndex.value].width / 2, 630)
-                )
+                drawImage(frames[frameIndex.value],
+                    dstSize = IntSize(size.width.toInt(), size.height.toInt()),
+                    )
             }
         }
     }
