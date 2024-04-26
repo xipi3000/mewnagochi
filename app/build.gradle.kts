d0e60d72eebfd3296487a03517d543fd015cf4e3
Ant <antoniolopez18022002@gmail.com>
2024-04-26 18:39:51 +0200

    added firebase to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,9 +85,7 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences-rxjava3:1.0.0")
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
-
     // When using the BoM, you don't specify versions in Firebase library dependencies
-
     // Add the dependency for the Firebase SDK for Google Analytics
     implementation("com.google.firebase:firebase-analytics-ktx")
 
