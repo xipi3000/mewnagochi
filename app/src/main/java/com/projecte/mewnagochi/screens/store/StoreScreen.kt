8d7fc1c7fdff59b6b757ca9ad7a24fe6ea967644
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-19 19:14:10 +0200

    llimpiant

--- app/src/main/java/com/projecte/mewnagochi/screens/store/StoreScreen.kt
+++ app/src/main/java/com/projecte/mewnagochi/screens/store/StoreScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -29,16 +30,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-
 import androidx.compose.material3.MaterialTheme
-
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -54,16 +51,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
-
 import com.projecte.mewnagochi.R
 import com.projecte.mewnagochi.services.storage.Item
-import kotlin.coroutines.coroutineContext
 
 
 @Composable
@@ -79,12 +73,16 @@ fun StoreScreen(
             .background(MaterialTheme.colorScheme.onBackground)
             .scrollable(rememberScrollState(), orientation = Orientation.Vertical)
     ) {
-        items(items) {
-            item ->
+        items(1){
             Button(onClick = storeViewModel::addMoney) {
                 Text(text = "+10")
             }
+        }
+        items(items) {
+            item ->
+            Spacer(modifier = Modifier.size(30.dp))
             StoreItem(item, storeViewModel::addItem)
+            
         }
     }
 }
