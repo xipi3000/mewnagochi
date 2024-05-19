cf680342bebb355b3bcb75d44638e76a497025b7
Pol Escolà Curcó <polescola3000@gmail.com>
2024-05-19 21:09:08 +0200

    diferent aproach al network

--- app/src/main/java/com/projecte/mewnagochi/MainActivity.kt
+++ app/src/main/java/com/projecte/mewnagochi/MainActivity.kt
@@ -8,6 +8,7 @@ import android.net.NetworkRequest
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -16,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,18 +29,37 @@ import com.projecte.mewnagochi.screens.profile.InternetPreferenceStateDataStore
 import com.projecte.mewnagochi.services.auth.NetworkConnection
 import com.projecte.mewnagochi.services.notification.MyFirebaseMessagingService
 import com.projecte.mewnagochi.services.storage.StorageServiceImpl
-import com.projecte.mewnagochi.stats.HealthConnectAvailability
 import com.projecte.mewnagochi.stats.HealthConnectManager
 import com.projecte.mewnagochi.stats.StatsViewModel
 import com.projecte.mewnagochi.ui.theme.MewnagochiTheme
 
 class MainActivity : ComponentActivity() {
+    private fun getNetworkType(networkCapabilities: NetworkCapabilities): String {
+        return when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> "WIFI"
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> "CELLULAR"
+            else -> "OTHER"
+        }
+    }
+    lateinit var context: Context
     private val networkCallback = object : ConnectivityManager.NetworkCallback() {
         // network is available for use
         override fun onAvailable(network: Network) {
             super.onAvailable(network)
-            Log.i("firestoreNetwork", "Connected to $network")
-            StorageServiceImpl().setFirestoreNetworkEnabled()
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+            val networkType = networkCapabilities?.let { getNetworkType(it) } ?: "UNKNOWN"
+            if(networkType=="WIFI"){
+                //Toast.makeText(context,"Wifi connected !",Toast.LENGTH_SHORT).show()
+                StorageServiceImpl().setFirestoreNetworkEnabled()
+                NetworkConnection.isAvailable=true
+            }
+            else if(networkType=="CELLULAR"){
+                //Toast.makeText(context,"Mobile connected !",Toast.LENGTH_SHORT).show()
+                StorageServiceImpl().setFirestoreNetworkDisabled()
+                NetworkConnection.isAvailable=false
+            }
+
         }
 
         // Network capabilities have changed for the network
@@ -50,22 +69,45 @@ class MainActivity : ComponentActivity() {
             networkCapabilities: NetworkCapabilities
         ) {
             super.onCapabilitiesChanged(network, networkCapabilities)
-            Log.i("firestoreNetwork",networkCapabilities.toString())
-            if(networkCapabilities.toString().contains("WIFI")){
-                StorageServiceImpl().setFirestoreNetworkEnabled()
-                NetworkConnection.isAvailable=true
-            }
-            else{
-                StorageServiceImpl().setFirestoreNetworkDisabled()
-                NetworkConnection.isAvailable=false
-            }
         }
 
         // lost network connection
         override fun onLost(network: Network) {
             super.onLost(network)
+            Toast.makeText(context,"No network operating !!",Toast.LENGTH_SHORT).show()
+        }
+    }
+    private val networkCallbackInfo = object : ConnectivityManager.NetworkCallback() {
+        // network is available for use
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+            val networkType = networkCapabilities?.let { getNetworkType(it) } ?: "UNKNOWN"
+            if(networkCapabilities.toString().contains("WIFI")){
+                Toast.makeText(context,"Wifi connected !",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(context,"Mobile connected !",Toast.LENGTH_SHORT).show()
+            }
+            Log.i("firestoreNetwork", "Connected to network: $network, Type: $networkType")
 
-            Log.i("firestoreNetwork", "Disconnected from $network")
+        }
+
+        // Network capabilities have changed for the network
+        @RequiresApi(Build.VERSION_CODES.S)
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            super.onCapabilitiesChanged(network, networkCapabilities)
+
+        }
+
+        // lost network connection
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            Toast.makeText(context,"No network operating !!",Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -81,8 +123,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connMgrInfo = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connMgrInfo.requestNetwork(networkRequest,networkCallbackInfo)
+        context = this
         setContent {
             MewnagochiTheme() {
+
                 val internetPreferenceState by this.InternetPreferenceStateDataStore.data.collectAsState(initial = InternetPreferenceState())
                 var isRegistered by remember { mutableStateOf(false)}
                 if(internetPreferenceState.internetPreferenceSelected==1){
